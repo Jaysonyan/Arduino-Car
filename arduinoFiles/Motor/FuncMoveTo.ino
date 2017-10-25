48794e518c8fe79a423b63c232b10841268cf2be
Haosen Han <h39han@uwaterloo.ca>
2017-10-25 17:11:17 -0400

    Update FuncMoveTo.ino

--- arduinoFiles/Motor/FuncMoveTo.ino
+++ arduinoFiles/Motor/FuncMoveTo.ino
@@ -1,9 +1,11 @@
+#define INCRE 1.0995574
 
 void MoveTo(){
-  int initSteps = encoder.getCountsAndResetM1();
+  int initSteps = encoders_get_counts_m1();
   int distance = sonar[0].ping_cm();
-  int numSteps = distance/INCRE;
-  while(numsteps != encoder.getCounts()){
+  int numSteps = (int)(distance/INCRE);
+  int finalSteps = initSteps+numSteps;
+  while(encoder_get_counts_m1() != numsteps){
     digitalWrite(12, HIGH); //Establishes forward direction of Channel A
     digitalWrite(9, LOW);   //Disengage the Brake for Channel A
     analogWrite(3, 255);   //Spins the motor on Channel A at full speed
