01d500962898adf6422c58bb8711de24d2cc9676
Jayson Yan <Jayson@v1020-wn-227-84.campus-dynamic.uwaterloo.ca>
2017-11-22 18:50:34 -0500

    Added comments

--- arduinoFiles/prototypeCode/main/main.ino
+++ arduinoFiles/prototypeCode/main/main.ino
@@ -2,28 +2,25 @@
 #include <NewPing.h>
 #include <math.h>
 
-//
+// Declaring all constants used in the code
 #define INCRE 3
-
 #define MAX_RANGE 50
-
 #define SENSOR1_TRIG 7
 #define SENSOR1_ECHO 10
-
 #define ENCODER_1A 2
 #define ENCODER_1B 4
 #define ENCODER_2A 5
 #define ENCODER_2B 6
-
 #define LED_GREEN 17
 #define LED_RED 18
 #define LED_BLUE 16
-
 #define SPEED_ADJUST_MULTIPLIER 5
 #define MEASURED_ENCODER_FULL_TURN 108.0
 
+//Sonar sensor object
 NewPing sonar = NewPing(SENSOR1_TRIG, SENSOR1_ECHO, MAX_RANGE);
 
+//Normalizes array of distances to hold preference to larger objects
 void takeMovingAverageInArray(int a[], int n){
   int *t = (int*)malloc(n*sizeof(int));
   for(int i = 0; i < n; i++){
@@ -36,6 +33,7 @@ void takeMovingAverageInArray(int a[], int n){
   t = NULL;
 }
 
+//Find closest distnace that is not zero
 int findIndexOfLowestNonZero(int a[], int n){
   int minIndex = -1;
   for(int i = 0; i < n; i++){
@@ -48,6 +46,7 @@ int findIndexOfLowestNonZero(int a[], int n){
   return minIndex;
 }
 
+//Rotates around 360 degrees in search of an object
 /*angle in degrees
 speed from 0 to 255
 direction true - clockwise, false - counterclockwise*/
@@ -107,6 +106,7 @@ int* rotate(float angle, int speed, bool direction, bool scan){
   }
 }
 
+//Takes in the distance and moves car that distance
 void moveTo(int speed, int distance){
   int numSteps = (int)(distance*INCRE);
   
@@ -168,39 +168,50 @@ void loop() {
   digitalWrite(LED_RED, 0);
   digitalWrite(LED_GREEN, 0);
   digitalWrite(LED_BLUE, 0);
-  
   int *distance;
+  //Rotates car around until it finds an object
   do{
     free(distance);
     distance = NULL;
     distance = rotate(360, 60, true, true);
     delay(1000);
   } while(findIndexOfLowestNonZero(distance, MEASURED_ENCODER_FULL_TURN) == -1);
-  
+
+  //Calibrates distance array to make zero-readings unfavourable to closest distance algorithm
   for(int i = 0; i < (int)(360.0/360.0 * MEASURED_ENCODER_FULL_TURN); i++){
     if(distance[i] == 0){
       distance[i] = MAX_RANGE;
     }
   }
-  
-  for(int i = 0; i < (int)(360.0/360.0 * MEASURED_ENCODER_FULL_TURN); i++){
+
+  //Prints distance array before moving average function
+  for(int i = 0; i < (int)(MEASURED_ENCODER_FULL_TURN); i++){
     Serial.print(distance[i]);
     Serial.print(",");
   }
   Serial.println();
+
   takeMovingAverageInArray(distance, MEASURED_ENCODER_FULL_TURN);
-  for(int i = 0; i < (int)(360.0/360.0 * MEASURED_ENCODER_FULL_TURN); i++){
+
+  //Prints distance array after moving average function
+  for(int i = 0; i < (int)(MEASURED_ENCODER_FULL_TURN); i++){
     Serial.print(distance[i]);
     Serial.print(",");
   }
   Serial.println();
+
+  //finds the index of the closest object in the distance array
   int minIndex = findIndexOfLowestNonZero(distance, MEASURED_ENCODER_FULL_TURN);
   Serial.println(minIndex);
   Serial.println(distance[minIndex]);
   Serial.println(((float)(MEASURED_ENCODER_FULL_TURN - minIndex))/MEASURED_ENCODER_FULL_TURN * 360.0);
+  
+  //calculates how much the car must turn to be facing the closest object
   float rotationRequired = ((float)(minIndex))/MEASURED_ENCODER_FULL_TURN * 360.0;
   rotate(360.0 - rotationRequired, 60, false, false);
   delay(1000);
+
+  //Determines an accurate distance reading of closest object
   int betterDistance = sonar.ping_cm();
   moveTo(60, max(betterDistance,distance[minIndex]));
 
